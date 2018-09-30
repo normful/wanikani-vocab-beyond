805a2df6c2fbad40d35ed07f25a65bc8d550c866
Norman Sue <normful@gmail.com>
2018-09-29 20:23:29 -0700

    [logger] Logger

---
+++ src/logger/logger.ts
@@ -0,0 +1,41 @@
+/* tslint:disable:no-console */
+
+const logPrefix = "[WKVB] ";
+
+export class Logger {
+  private prefix: string;
+  private disableLogging: boolean;
+
+  constructor(disableLogging: boolean) {
+    this.prefix = logPrefix;
+    this.disableLogging = disableLogging;
+  }
+
+  public debug(msg: string, ...args: any[]) {
+    if (this.disableLogging) {
+      return;
+    }
+    console.debug(this.prefix + msg, ...args);
+  }
+
+  public info(msg: string, ...args: any[]) {
+    if (this.disableLogging) {
+      return;
+    }
+    console.log(this.prefix + msg, ...args);
+  }
+
+  public warn(msg: string, ...args: any[]) {
+    if (this.disableLogging) {
+      return;
+    }
+    console.warn(this.prefix + msg, ...args);
+  }
+
+  public error(msg: string, ...args: any[]) {
+    if (this.disableLogging) {
+      return;
+    }
+    console.error(this.prefix + msg, ...args);
+  }
+}
