89af2e2cfab79b89bd03e231ed86558129d0b68e
Norman Sue <normful@gmail.com>
2018-09-30 10:57:10 -0700

    [forvo] getWordPronunciations implemented with gm-http

---
+++ src/forvo/getWordPronunciations.ts
@@ -0,0 +1,42 @@
+import http from "gm-http";
+import { Logger } from "../logger/logger";
+import { IWKOFSettings } from "../wkof/wkofConstants";
+
+const Log = new Logger(false);
+
+export function getWordPronunciations(
+  jpWord: string,
+  settings: IWKOFSettings
+): Promise<any> {
+  if (!jpWord) {
+    return Promise.reject(new Error("jpWord missing"));
+  }
+
+  const forvoApiKey = settings ? settings.forvo_api_key : null;
+
+  if (!forvoApiKey) {
+    return Promise.reject(new Error("Forvo api key missing"));
+  }
+
+  const forvoUrl =
+    "https://apifree.forvo.com/key/" +
+    forvoApiKey +
+    "/format/json" +
+    "/action/word-pronunciations" +
+    "/word/" +
+    encodeURIComponent(jpWord) +
+    "/language/ja" +
+    "/rate/" +
+    String(settings.forvo_min_rating || 0) +
+    "/country/JPN" +
+    "/order/rate-desc";
+
+  return http
+    .get(forvoUrl)
+    .then((res: Response$) => {
+      return Promise.resolve(res.responseText);
+    })
+    .catch(err => {
+      Log.error("Forvo error: ", err);
+    });
+}
